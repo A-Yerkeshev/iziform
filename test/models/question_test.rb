2dbde43c0c9a9366d65f5a4b3cba82e358392c72
Arman Yerkesh <a.yerkesh@gmail.com>
2022-02-22 09:46:10 +0200

    Write test to save question to database

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Should save queston" do
+    q = Question.new(form_id: 1, content: 'Left, right or center?', options: ['left', 'right', 'center'], question_type: 'radiobutton')
+    assert q.save
+  end
 end
