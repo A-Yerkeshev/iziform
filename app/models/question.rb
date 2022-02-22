e684b5793cfd64cb49f46c59d5f3d1a82c9a2b28
Arman Yerkesh <a.yerkesh@gmail.com>
2022-02-22 10:23:25 +0200

    Add form_id validation to Question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,5 @@
 class Question < ApplicationRecord
+	validates :form_id, presence: true, numericality: { only_integer: true }
+
 	serialize :options, Array
 end
--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,8 +1,22 @@
 require "test_helper"
 
 class QuestionTest < ActiveSupport::TestCase
+  def setup
+    @q = Question.new(form_id: 1, content: 'Left, right or center?', options: ['left', 'right', 'center'], question_type: 'radiobutton')
+  end
+
   test "Should save queston" do
-    q = Question.new(form_id: 1, content: 'Left, right or center?', options: ['left', 'right', 'center'], question_type: 'radiobutton')
-    assert q.save
+    assert @q.save
+  end
+
+  test "Form id should be valid integer" do
+    @q.form_id = nil
+    assert_not @q.valid?
+
+    @q.form_id = 'string'
+    assert_not @q.valid?
+
+    @q.form_id = 0.98
+    assert_not @q.valid?
   end
 end
