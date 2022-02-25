6dc3aafc3f81d9edc0fddd17c233046e262fd89e
Arman Yerkesh <a.yerkesh@gmail.com>
2022-02-25 11:03:42 +0200

    Revert "Change find_by to find_all in Responses controllers set_questions function"
    This reverts commit a6b9e68deae1743de32f72fea23a96452d978bc4.

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -66,7 +66,7 @@ class ResponsesController < ApplicationController
 
     # Get questions array corresponding to form_id provided
     def set_questions
-      @questions = Question.find_all(form_id: params[:form_id])
+      @questions = Question.find_by(form_id: params[:form_id])
     end
 
     # Only allow a list of trusted parameters through.
