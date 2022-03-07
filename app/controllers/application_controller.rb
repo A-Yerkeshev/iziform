30ceab6438a5370bc2bf7ea35f259fbf8a830f90
Arman Yerkesh <a.yerkesh@gmail.com>
2022-03-07 11:22:45 +0200

    Add basic jumbotron on root page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  def index
+    @forms_count = Form.count
+    @responses_count = Response.count
+
+    render template: 'home.html.slim'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root 'application#index'
+
   resources :responses
   resources :forms
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
