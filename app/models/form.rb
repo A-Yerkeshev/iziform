45be6a4f97905ef3c0d6f7a4435ea429bfa46ad5
Arman Yerkesh <a.yerkesh@gmail.com>
2022-03-11 13:59:59 +0200

    Configure Cocoon

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,4 +1,5 @@
 class Form < ApplicationRecord
   has_many :responses, dependent: :destroy
-  has_many :questions, dependent: :destroy
+  has_many :questions,inverse_of: :form, dependent: :destroy
+  accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true
 end
