89b3f6d69a3e9ab56931ccf919ca7c9db6e14f69
Arman Yerkesh <a.yerkesh@gmail.com>
2022-03-18 09:41:18 +0200

    Add form name validation

--- app/models/form.rb
+++ app/models/form.rb
@@ -2,4 +2,6 @@ class Form < ApplicationRecord
   has_many :responses, dependent: :destroy
   has_many :questions,inverse_of: :form, dependent: :destroy
   accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true
+
+  validates :name, presence: true, length: { maximum: 100 }
 end
